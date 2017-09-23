3964db610dc779346902046e195798afe8c0ddb6
apktool <apktoolOo@gmail.com>
2017-09-23 09:58:22 +0800

    Shell script of centos installation have done

--- scripts/argsAuth.sh
+++ scripts/argsAuth.sh
@@ -3,8 +3,8 @@
 args=$1
 grepresult=`grep "motivation" group_vars/all | awk '{print $2}'`
 
-if [ ${args} != "start" -a ${args} != "stop" ]; then
-	echo "Parameter Error: start|stop"
+if [ ${args} != "start" -a ${args} != "stop" -a ${args} != "install" ]; then
+	echo "Parameter Error: start|stop|install"
 	exit 1
 fi
 
@@ -14,4 +14,16 @@ fi
 
 if [ ${args} = "stop" ]; then
 	sed -i "s/${grepresult}/stop/g" group_vars/all
+fi
+
+if [ ${args} = "install" ]; then
+	if [ -z $2 ]; then
+		echo "Lack of Parameter: centos|hadoop"
+		exit 1
+	elif [ $2 != "centos" -a $2 != "hadoop" ]; then
+		echo "Parameter Error after install: centos|hadoop"
+		exit 1
+	else
+		sed -i "s/${grepresult}/install/g" group_vars/all
+	fi
 fi
--- shell.sh
+++ shell.sh
@@ -3,13 +3,18 @@
 args=$1
 
 if [ ! -z ${args} ]; then
-	bash scripts/argsAuth.sh ${args}
+	bash scripts/argsAuth.sh $@
 else
 	echo "Usage: $0 <start|stop>"
 	exit 1
 fi
 
+
 if [ $? -eq 0 ]; then
-	expect -f scripts/hadoopManage.exp ${args}
+	if [ ${args} = "start" -o ${args} = "stop" ]; then
+		expect -f scripts/hadoopManage.exp ${args}
+	elif [ ${args} = "install" ]; then
+		expect -f scripts/install.exp $2
+	fi
 fi
 
